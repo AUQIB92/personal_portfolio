f33accff0c37a8ec032747757cf5387efc910d18
AUQIB92 <auqib@gmail.com>
2024-09-26 12:25:19 +0530

    Added title

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
-'use client'
-import Head from 'next/head';
-import ProfessorLandingPage from '../components/ProfessorLandingPage';
+"use client";
+import Head from "next/head";
+import ProfessorLandingPage from "../components/ProfessorLandingPage";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Personal Portfolio</title>
-        <meta name="description" content="Personal portfolio website of Assistant Professor John Doe" />
+        <meta
+          name="description"
+          content="Personal portfolio website of Assistant Professor Auqib Hamid Lone"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ProfessorLandingPage />
